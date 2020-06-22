f4a1fba56fab59ab85a96ea5a809ab0c532f4070
Anton Bondarenko <ABandarenka@gomel.iba.by>
2020-06-22 15:02:00 +0300

    task 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <div className="cardWrapper">
-        <Card name="Caption">
+        <Card title="Caption">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
             enim ad minim veniam, quis nostrud exercitation ullamco laboris 
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 
 const Card = (props) => {
+
+  // set font color by default
+  const [colorState, setColorState] = useState({
+    color: 'inherit'
+  });
+
+  // swithc color by checkbox
+  const swichColorHandler = () => {
+    colorState.color === 'inherit' ? setColorState({ color: '#63ce5a' }) : setColorState({ color: 'inherit' });
+  };
+
   return (
-    <div className="Card">
-      <h1>{props.name}</h1>
+    <div className="Card" style={colorState}>
+      <div className="Title">
+        <h1>{props.title}</h1>
+        <div className="Switch">
+          <label htmlFor="switchColor">Switch color:</label>
+          <input type="checkbox" id="switchColor" onChange={swichColorHandler}/>
+        </div>
+      </div>
       <hr/>
       <p>{props.children}</p>
     </div>
