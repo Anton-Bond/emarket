e24f217ce7a440d6bdd51d699ac3dbe689719315
Anton Bondarenko <ABandarenka@gomel.iba.by>
2020-06-26 21:07:37 +0300

    task 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="cardWrapper">
-        <Card title="Caption">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-            nisi ut aliquip ex ea commodo consequat.
-        </Card>
-      </div>      
-    </div>
-  );
+class App extends Component {
+  state = {
+    card: {
+      title: 'Caption',
+      oldTitle: '',
+      context: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Utenim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat.`,
+      oldContext: '',
+      isEditMode: false,
+      isChecked: false
+    }
+  }
+
+  // swithch card text color
+  checkedHandler = () => {
+    const card = this.state.card;
+    card.isChecked = !card.isChecked;
+    this.setState({ card });
+  }
+
+  // binding with input of title in editor mode
+  titleChangedHandler = event => {
+    const card = this.state.card;
+    card.title = event.target.value;
+    this.setState({ card });
+  }
+
+  // binding with input of context in editor mode
+  contextChangeHandler = event => {
+    const card = this.state.card;
+    card.context = event.target.value;
+    this.setState({ card });
+  }
+
+  // swith to editor mode
+  toggleModeHandler = () => {
+    const card = this.state.card;
+    card.oldTitle = card.title;
+    card.isChecked = false;
+    card.oldContext = card.context;
+    card.isEditMode = !card.isEditMode;
+    this.setState({ card });
+  }
+
+  // cancel changes in editor mode
+  cancelChangeHandler = () => {
+    const card = this.state.card;
+    card.title = card.oldTitle;
+    card.context = card.oldContext;
+    card.isEditMode = !card.isEditMode;
+    this.setState({ card });
+  }
+  
+
+  render () {
+    const card = this.state.card;
+    return (
+      <div className="App">
+        <Header />
+        <div className="cardWrapper">
+          <Card 
+            title={card.title}
+            context={card.context}
+            isChecked={card.isChecked}
+            isEditMode={card.isEditMode}
+            onCheck={this.checkedHandler}
+            changedTitle={this.titleChangedHandler}
+            changedContext={this.contextChangeHandler}
+            onToggleMode={this.toggleModeHandler}
+            onCancel={this.cancelChangeHandler}
+          />
+        </div>   
+      </div>
+      
+    );
+  }
 }
 
 export default App;
--- src/Card/index.js
+++ src/Card/index.js
@@ -1,25 +1,51 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { AiOutlineEdit } from "react-icons/ai";
+import { AiOutlineCheck } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import './Card.css';
 
 const Card = (props) => {
-
-  const [isChecked, setChecked] = useState(false);
-
   return (
-    <div className="Card" style={{ color: isChecked && '#63ce5a' }}>
-      <div className="Title">
-        <h1>{props.title}</h1>
-        <div className="Switch">
-          <label htmlFor="switchColor">Switch color:</label>
-          <input
-            type="checkbox"
-            id="switchColor"
-            onChange={() => setChecked(!isChecked)}
-          />
+    // style depends on checkbox
+    <div className="Card" style={{ color: props.isChecked && '#63ce5a' }}>
+      { props.isEditMode 
+        // editor card title
+        ? <div className="Title">
+            <div className="Title-context">
+              <input type="text" id="title-editor" onChange={props.changedTitle} value={props.title} />
+            </div>
+            <div className="Controls">
+              <a onClick={props.onToggleMode}><AiOutlineCheck /></a>
+              <a onClick={props.onCancel}><AiOutlineClose /></a>
+            </div>
+          </div>
+          // view title card
+        : <div className="Title">
+          <div className="Title-context">
+            <h1>{props.title}</h1>
+          </div>
+            <div className="Controls">
+              <a onClick={props.onToggleMode}><AiOutlineEdit /></a>
+              {/* color switcher */}
+              <input
+                type="checkbox"
+                id="switchColor"
+                onChange={props.onCheck}
+              />
+            </div>
         </div>
-      </div>
+      }
       <hr />
-      <p>{props.children}</p>
+      { props.isEditMode 
+        // editor context card
+        ? <div>
+            <textarea onChange={props.changedContext} value={props.context}></textarea>
+          </div>
+          // view contex cart
+        : <div>
+            <p>{props.context}</p>
+          </div>
+      }
     </div>
   );
 }
