a580ef0f594d3d996ef049d39037fc0a80709fbf
Anton Bondarenko <ABandarenka@gomel.iba.by>
2020-07-25 11:37:54 +0300

    task 7 changes

--- src/AddCard/index.js
+++ src/AddCard/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { AiOutlineCheck } from 'react-icons/ai';
 import { AiOutlineClose } from 'react-icons/ai';
-import { AiOutlinePlus } from 'react-icons/ai';
+import { AiOutlinePlusCircle } from 'react-icons/ai';
 import { v1 as uuidv1 } from 'uuid';
 
 import './AddCard.css';
 
-class Card extends Component {
+class AddCard extends Component {
   state = {
     id: null,
     title: '',
@@ -45,40 +45,40 @@ class Card extends Component {
   }
 
   render() {
-    return (
+    return this.state.id ? (
       <div className="AddCard">
-        { this.state.id ?
-          (
-            <div>
-              <div className="title-add-card">
-                <div className="title-context-add-card">
-                  <input
-                    type="text"
-                    id="title-editor"
-                    className="editor-title"
-                    onChange={this.titleChangedHandler}
-                    value={this.state.title}
-                  />
-                </div>
-                <div className="controls-add-card">
-                  <AiOutlineCheck onClick={this.saveHandler} />
-                  <AiOutlineClose
-                    onClick={() => this.setState({ id: null })}
-                  />
-                </div>
-              </div>
-              <hr />
-              <textarea
-                className="textarea-add-card"
-                onChange={this.contextChangedHandler}
-                value={this.state.context}
-              ></textarea>
-            </div>
-          ) : <AiOutlinePlus className="icon-add" onClick={() => this.setState({ id: uuidv1() })}/>
-        }
+        <div className="title-add-card">
+          <div className="title-context-add-card">
+            <input
+              type="text"
+              id="title-editor"
+              className="editor-title"
+              onChange={this.titleChangedHandler}
+              value={this.state.title}
+            />
+          </div>
+          <div className="controls-add-card">
+            <AiOutlineCheck onClick={this.saveHandler} />
+            <AiOutlineClose onClick={() => this.setState({ id: null })} />
+          </div>
+        </div>
+        <hr />
+        <textarea
+          className="textarea-add-card"
+          onChange={this.contextChangedHandler}
+          value={this.state.context}
+        ></textarea>
+      </div>
+    ) : (
+      <div
+        className="AddCard"
+        style={{ cursor: 'pointer' }}
+        onClick={() => this.setState({ id: uuidv1() })}
+      >
+        <AiOutlinePlusCircle className="icon-add" />
       </div>
     );
   }
 }
 
-export default Card;
+export default AddCard;
